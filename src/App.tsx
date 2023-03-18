fb7727e8843aac4d6bd4924e87c19e8a0e5bb496
Spink994 <ayodeji.bakare@sourcemygadgets.com>
2023-03-18 16:45:14 +0100

    Project Setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+
+  return (
+    <div className="App">
+     <p>My First SCSS Project</p>
+     <button className='btn_variant_3'>Log in</button>
+    </div>
+  )
+}
+
+export default App
